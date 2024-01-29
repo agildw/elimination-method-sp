6eb2dd90263ccc8624cf974fc5d980ef4279b0e8
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-01-29 22:02:54 +0700

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center h-16 text-gray-500 font-semibold">
+      <p className="text-center">Made with ❤️ by kelompok 1</p>
+    </footer>
+  );
+};
+
+export default Footer;
