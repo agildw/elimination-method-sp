906ed9f27b3f944b04445a2522721480805d1aae
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-02-01 08:02:56 +0700

    add steps

--- app/utils/solver.ts
+++ app/utils/solver.ts
@@ -1,16 +1,40 @@
-type Matrix = number[][];
+export type Matrix = number[][];
 
-function gaussianElimination(input: string): number[] {
-  const matrix = parseInput(input);
+export interface Solution {
+  value: number;
+  variable: string;
+  steps: string[];
+}
+
+export interface Step {
+  matrix: Matrix;
+  step: string;
+}
+
+export interface GaussianEliminationResult {
+  eliminationSteps: Step[];
+  solution: Solution[];
+  equations: string[];
+  inputMatrix: Matrix;
+}
+
+function gaussianElimination(input: string): GaussianEliminationResult {
+  const { matrix, equations } = parseInput(input);
+  const inputMatrix = JSON.parse(JSON.stringify(matrix));
+  const steps: Step[] = [];
   const n = matrix.length;
 
   // Mengonversi matriks menjadi bentuk segitiga atas
   for (let i = 0; i < n; i++) {
-    // Normalisasi baris saat ini
+    // Menyederhanakan baris saat ini
     const factor = matrix[i][i];
     for (let k = i; k < n + 1; k++) {
       matrix[i][k] /= factor;
     }
+    steps.push({
+      matrix: JSON.parse(JSON.stringify(matrix)),
+      step: `Sederhanakan baris ${i + 1} dengan pembagi ${factor}`,
+    });
 
     // Mengeliminasi elemen di bawah baris saat ini
     for (let j = i + 1; j < n; j++) {
@@ -18,31 +42,57 @@ function gaussianElimination(input: string): number[] {
       for (let k = i; k < n + 1; k++) {
         matrix[j][k] -= factor * matrix[i][k];
       }
+      steps.push({
+        matrix: JSON.parse(JSON.stringify(matrix)),
+        step: `Eliminasi baris ${j + 1} menggunakan baris ${i + 1}`,
+      });
     }
   }
 
-  return backSubstitution(matrix);
+  const solution = backSubstitution(matrix);
+
+  return {
+    eliminationSteps: steps,
+    solution: solution,
+    equations,
+    inputMatrix,
+  };
 }
 
-function backSubstitution(matrix: Matrix): number[] {
+function backSubstitution(matrix: Matrix): Solution[] {
   const n = matrix.length;
-  const solution = new Array(n).fill(0);
+  const solution: Solution[] = new Array(n).fill(0);
 
   for (let i = n - 1; i >= 0; i--) {
+    const variable = String.fromCharCode(120 + i);
+    const steps: string[] = [];
+    steps.push(`Substitusi ${variable}`);
+
     let sum = 0;
     for (let j = i + 1; j < n; j++) {
-      sum += matrix[i][j] * solution[j];
+      sum += matrix[i][j] * solution[j].value;
     }
-    solution[i] = matrix[i][n] - sum;
+
+    steps.push(`${variable} = ${matrix[i][n]} - ${sum}`);
+    steps.push(`${variable} = ${matrix[i][n] - sum}`);
+
+    solution[i] = {
+      value: matrix[i][n] - sum,
+      variable,
+      steps,
+    };
   }
 
   return solution;
 }
 
 // fungsi untuk mengonversi input menjadi matriks
-function parseInput(input: string): Matrix {
+function parseInput(input: string): {
+  matrix: Matrix;
+  equations: string[];
+} {
   // Memisahkan persamaan dalam input
-  const equations = input.split(";");
+  const equations = input.split(";").map((equation) => equation.trim());
   const matrix: Matrix = [];
 
   for (const equation of equations) {
@@ -89,10 +139,14 @@ function parseInput(input: string): Matrix {
     }
 
     row.push(parseInt(rhs, 10)); // Menambahkan bagian kanan sebagai elemen terakhir
+
     matrix.push(row);
   }
 
-  return matrix;
+  return {
+    matrix,
+    equations,
+  };
 }
 
 export default gaussianElimination;
