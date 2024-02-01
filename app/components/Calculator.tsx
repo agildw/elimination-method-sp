c6612e7384415af8a5fdfdbedcf4506a4316670d
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-02-01 08:02:45 +0700

    add steps

--- app/components/Calculator.tsx
+++ app/components/Calculator.tsx
@@ -1,25 +1,149 @@
 "use client";
 import { useState } from "react";
-import gaussianElimination from "../utils/solver";
+import gaussianElimination, {
+  GaussianEliminationResult,
+  type Matrix,
+  Step,
+} from "../utils/solver";
+
+const Matrix = ({ matrix }: { matrix: Matrix }) => {
+  return (
+    <table className="matrix w-full max-w-40">
+      <tbody>
+        {matrix.map((row, i) => (
+          <tr key={i}>
+            {row.map((col, j) => (
+              <td
+                key={j}
+                className={`w-10 text-center font-base text-xl ${
+                  j === row.length - 1 ? "w-20" : ""
+                }`}
+              >
+                {col}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const EliminationSolutionStep = ({ step }: { step: Step }) => {
+  return (
+    <div className="flex flex-col space-y-4">
+      <p className="text-gray-500 text-lg font-base text-left">{step.step}</p>
+      <Matrix matrix={step.matrix} />
+    </div>
+  );
+};
+
+const SubstitutionSolutionStep = ({ step }: { step: string[] }) => {
+  return (
+    <div className="flex flex-col space-y-4">
+      {step.map((eq, i) => (
+        <p key={i} className="text-gray-500 text-xl font-base text-left">
+          {eq}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const StepByStep = ({
+  eliminationSteps,
+  substitutionSteps,
+}: {
+  eliminationSteps: Step[];
+  substitutionSteps: string[][];
+}) => {
+  return (
+    <div className="flex flex-col space-y-8">
+      <p className="text-gray-500 text-xl font-bold text-left">
+        1. Eliminasi maju
+      </p>
+      <div className="flex flex-col space-y-8">
+        {eliminationSteps.map((step, i) => (
+          <EliminationSolutionStep key={i} step={step} />
+        ))}
+      </div>
+      <p className="text-gray-500 text-xl font-bold text-left">
+        2. Subtitusi mundur
+      </p>
+      <div className="flex flex-col space-y-8">
+        {substitutionSteps.map((step, i) => (
+          <SubstitutionSolutionStep key={i} step={step} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Solution = ({ result }: { result: GaussianEliminationResult }) => {
+  return (
+    <div className="flex flex-col space-y-8">
+      <div className="flex flex-col space-y-4">
+        <p className="text-gray-500 text-2xl font-bold text-left">Persamaan</p>
+        {result.equations.map((eq, i) => (
+          <p key={i} className="text-gray-500 text-xl font-base text-left">
+            {eq}
+          </p>
+        ))}
+        <p className="text-gray-500 text-2xl font-bold text-left">Matriks</p>
+        <Matrix matrix={result.inputMatrix} />
+      </div>
+      <div className="flex flex-col space-y-4">
+        <p className="text-gray-500 text-2xl font-bold text-left mt-8">Hasil</p>
+        <code className="w-full flex-col items-center rounded-lg bg-gray-800 p-4 pl-6 text-left text-sm text-white sm:text-base">
+          <span className="flex gap-4">
+            <span>
+              {result.solution.map((res, i) => (
+                <span key={i} className="text-gray-100 text-xl">
+                  {res.variable} = {res.value}
+                  <br />
+                </span>
+              ))}
+            </span>
+          </span>
+        </code>
+        <div
+          tabIndex={0}
+          className="collapse collapse-arrow border border-base-300 bg-base-200"
+        >
+          <div className="collapse-title text-xl font-medium">
+            Langkah-langkah
+          </div>
+          <div className="collapse-content">
+            <StepByStep
+              eliminationSteps={result.eliminationSteps}
+              substitutionSteps={result.solution
+                .toReversed()
+                .map((s) => s.steps)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Calculator = () => {
   const [equation, setEquation] = useState("");
-  const [result, setResult] = useState<number[] | null>(null);
+  const [result, setResult] = useState<GaussianEliminationResult | null>(null);
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
     setError("");
     setResult(null);
-    setLoading(true);
+
     try {
       const res = gaussianElimination(equation);
+      console.log(res);
       setResult(res);
     } catch (e: any) {
       console.log(e);
       setError("Persamaan tidak valid");
     }
-    setLoading(false);
   };
   return (
     <div className="flex flex-col justify-center p-4 max-w-6xl w-full space-y-12">
@@ -54,42 +178,7 @@ const Calculator = () => {
           Pisahkan dengan tanda titik koma (;)
         </p>
       </div>
-      {!result && !error && !loading && (
-        <div className="flex flex-col space-y-4">
-          <p className="text-gray-500 text-2xl font-bold text-left">
-            Contoh (Klik untuk memasukkan)
-          </p>
-          <button
-            className="text-lg bg-gray-800 hover:bg-gray-600 text-white font-bold py-4 px-4 rounded-lg w-full text-left tracking-widest"
-            onClick={() => setEquation("x+2y-3z=1;2x-y+z=1;x+4y-2z=9")}
-          >
-            x+2y-3z=1;2x-y+z=1;x+4y-2z=9
-          </button>
-        </div>
-      )}
 
-      {loading && <div className="skeleton h-32 w-full" />}
-
-      {result && (
-        <div className="flex flex-col space-y-4">
-          <p className="text-gray-500 text-2xl font-bold text-left">Hasil</p>
-          <code className="w-full flex-col items-center rounded-lg bg-gray-800 p-4 pl-6 text-left text-sm text-white sm:text-base">
-            <span className="flex gap-4">
-              <span>
-                {/* <span className="text-gray-100 text-xl">
-                [{result.join(", ")}]{" "}
-              </span> */}
-                {result.map((res, i) => (
-                  <span key={i} className="text-gray-100 text-xl">
-                    x<sub>{i + 1}</sub> = {res}
-                    <br />
-                  </span>
-                ))}
-              </span>
-            </span>
-          </code>
-        </div>
-      )}
       {error && (
         <div
           role="alert"
@@ -111,6 +200,23 @@ const Calculator = () => {
           <span>{error}</span>
         </div>
       )}
+
+      {!result && (
+        <div className="flex flex-col space-y-4">
+          <p className="text-gray-500 text-2xl font-bold text-left">Contoh</p>
+          <button
+            className="text-lg bg-gray-800 hover:bg-gray-600 text-white font-semibold py-4 px-4 rounded-md w-full text-left"
+            onClick={() => setEquation("2x + 4y = 28 ; 3x + 2y = 22")}
+          >
+            2x + 4y = 28; 3x + 2y = 22
+          </button>
+          <p className="text-gray-500 text-md font-light text-left">
+            Klik untuk memasukkan contoh persamaan
+          </p>
+        </div>
+      )}
+
+      {result && <Solution result={result} />}
     </div>
   );
 };
