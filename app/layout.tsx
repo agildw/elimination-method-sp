df078e033527108160100266f5c5ebfd8a087887
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-02-29 21:53:56 +0700

    change page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tugas Besar 1 -  Aljabar Linier",
+  title: "Gaussian Elimination Method With Back Substitution",
   description:
     "This is a website that explains Gaussian Elimination Method With Back Substitution",
 };
