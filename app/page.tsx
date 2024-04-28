cdb548e5acde9b0a3ee165851fff6bc7e83228bc
Agil Dwiki Yudistira <yudistiraagil24@gmail.com>
2024-04-28 14:54:16 +0700

    hide our team components

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center space-y-36">
       <SectionOne />
-      <SectionTwo />
+      {/* <SectionTwo /> */}
       <SectionThree />
       <SectionFour />
       {/* <SectionFive /> */}
